a64ba3bd3a04c6f1e255d816df5b83017bca396f
Sandro Lain <sandrolain@outlook.com>
2022-12-30 15:15:46 +0100

    Added URI password extraction

--- pkg/pwdutils/pwdutils.go
+++ pkg/pwdutils/pwdutils.go
@@ -1,6 +1,9 @@
 package pwdutils
 
 import (
+	"fmt"
+	"net/url"
+
 	pwdgen "github.com/sethvargo/go-password/password"
 	pwdval "github.com/wagslane/go-password-validator"
 )
@@ -17,4 +20,16 @@ func Generate(len int) (string, error) {
 	dig := len / 4
 	sim := len / 4
 	return pwdgen.Generate(len, dig, sim, false, false)
+}
+
+func ExtractPasswordFromURI(uri string) (pwd string, err error) {
+	u, err := url.Parse(uri)
+	if err != nil {
+		return
+	}
+	pwd, ok := u.User.Password()
+	if !ok {
+		err = fmt.Errorf("the URI does not contain a password")
+	}
+	return
 }
